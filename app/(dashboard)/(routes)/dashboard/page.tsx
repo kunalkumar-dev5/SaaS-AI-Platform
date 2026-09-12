b0e94d8e228e3c8ab4b84bcaf6b9a7f37b0cae17
Kunal Kumar <kunalkr5674@gmail.com>
2026-09-12 20:23:46 +0530

    add Dashboard

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default DashboardPage;
